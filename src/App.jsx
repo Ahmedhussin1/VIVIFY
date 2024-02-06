25c093f769adb3184548d8d03b2d51cc2f67c25b
Ahmed Hussin <ahmed208087@gmail.com>
2024-02-06 12:47:37 +0200

    adding base to the vite.config file to point to the site link

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from './Components/Navbar/Navbar'
+import Footer from './Components/Footer/Footer';
 // pages imports
 import Home from './Pages/Home page/Home';
 import AboutPage from './Pages/About page/AboutPage';
 import ContactPage from './Pages/Contact Page/ContactPage';
 import Projects from './Pages/Projects/Projects';
-import Footer from './Components/Footer/Footer';
 function App() {
   return (
    <div>
